964ad97df42cdbec9e52fe20e4062b28a3928472
leosmaia21 <leosmaia21@gmail.com>
2025-06-22 20:13:05 +0100

    test.py created

--- make_py.py
+++ make_py.py
@@ -94,22 +94,3 @@ class Target:
 
           
 
-action1 = Target(["t"],["file3"])
-action2 = Target(["file3"],["file2"])
-action3 = Target(["file2"],["file1"])
-
-@action1.add_action
-def t_Action():
-  print("t")
-
-@action2.add_action
-def file3_Action():
-  print("file3")
-  os.system("touch file3")
-
-@action3.add_action
-def file2_Action():
-  print("file2")
-  os.system("touch file2")
-
-action1()
